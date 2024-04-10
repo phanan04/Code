cd704f1a2b629335c733be7e112d5eb698ec383f
Phan An <an2004hy@gmail.com>
2024-04-11 01:23:26 +0700

    commit

--- Java/array.java
+++ Java/array.java
@@ -3,14 +3,14 @@ import java.util.Arrays;
 
 public class array {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
         int[] a = new int[size];
         for (int i = 0; i < size; i++) {
-            a[i] = scanner.nextInt();
+            a[i] = sc.nextInt();
         }
         Arrays.sort(a);
         System.out.println(Arrays.toString(a));
     }
 }
-sdfjklsdjkl
+
--- Java/phuontrinhmotan.java
+++ Java/phuontrinhmotan.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class test {
+public class phuontrinhmotan {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
