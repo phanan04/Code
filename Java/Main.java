e6589e497903eeb29a9e5e674c7b267c6b877700
Phan An <an2004hy@gmail.com>
2024-04-17 22:45:52 +0700

    .

--- Java/Main.java
+++ Java/Main.java
@@ -1,10 +1,8 @@
 public class Main {
+  int x = 5;
+
   public static void main(String[] args) {
-    for (int i = 0; i < 10; i++) {
-      if (i == 4) {
-        continue;
-      }
-      System.out.println(i);
-    }
+    Main myObj = new Main();
+    System.out.println(myObj.x);
   }
 }
